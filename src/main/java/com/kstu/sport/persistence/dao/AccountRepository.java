49e56478dc30de690e25c7e58b81ac4870fa8a29
e.ivanov <eugeneiy1847@gmail.com>
2020-09-20 00:23:04 +0300

    add TODO

--- src/main/java/com/kstu/sport/persistence/dao/AccountRepository.java
+++ src/main/java/com/kstu/sport/persistence/dao/AccountRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account,Long> {
     Account findByEmail(String email);
 
+    //TODO обязательно сделать поиск независимо от регистра.Мб можно сделать через репу,
+    // на крайняк criteriaApi или sql @Query
     Long countAccountByEmail(String email);
 }
